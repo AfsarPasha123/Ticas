9e21fac0c8fbd6085ab87771b9b0d6b49e1962b7
pt-nikhil <Nikhil.saini@pangeatech.net>
2025-02-24 14:51:19 +0530

    fix: update profile_image column size to VARCHAR(254) and remove redundant migration

--- dist/database/migrations/003_add_profile_image.sql
+++ dist/database/migrations/003_add_profile_image.sql
@@ -1,6 +1,6 @@
 -- Add profile_image column to users table
 ALTER TABLE users
-ADD COLUMN profile_image VARCHAR(1024);
+ADD COLUMN profile_image VARCHAR(254);
 
 -- Migration Down
 -- ALTER TABLE users DROP COLUMN profile_image;
