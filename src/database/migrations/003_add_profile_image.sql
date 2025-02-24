9b33fbf9e89740831314df45ccbc6f5cfdebce6c
pt-nikhil <Nikhil.saini@pangeatech.net>
2025-02-24 16:39:51 +0530

    fix: change in env

--- src/database/migrations/003_add_profile_image.sql
+++ src/database/migrations/003_add_profile_image.sql
@@ -1,6 +1,6 @@
 -- Add profile_image column to users table
 ALTER TABLE users
-ADD COLUMN profile_image VARCHAR(254);
+ADD COLUMN profile_image VARCHAR(1024);
 
 -- Migration Down
 -- ALTER TABLE users DROP COLUMN profile_image;
